902097314825e673db0b637db7d4de41909a523b
Tan Chia Qian <66521590+TCQian@users.noreply.github.com>
2026-03-10 23:10:31 +0800

    Added bash script to run experiments

---
+++ run_sbatch.sh
@@ -0,0 +1,9 @@
+dataset=("HoneyBee" "Beauty" "Jockey")
+testing_gaussians_1frames=(25000 35000)
+for i in "${!dataset[@]}"; do
+    for j in "${!testing_gaussians_1frames[@]}"; do
+        sbatch run_gaussianimage.sh \
+            --data_name "${dataset[$i]}" \
+            --num_points "${testing_gaussians_1frames[$j]}" 
+    done
+done
