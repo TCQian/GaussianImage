2eac2f9f61f1c7dd2d340a6174083bd08128e77f
Tan Chia Qian <66521590+TCQian@users.noreply.github.com>
2026-03-10 23:12:22 +0800

    Added arguments

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -4,6 +4,8 @@ for i in "${!dataset[@]}"; do
     for j in "${!testing_gaussians_1frames[@]}"; do
         sbatch run_gaussianimage.sh \
             --data_name "${dataset[$i]}" \
-            --num_points "${testing_gaussians_1frames[$j]}" 
+            --num_points "${testing_gaussians_1frames[$j]}" \
+            --start_frame 0 \
+            --num_frames 50
     done
 done
